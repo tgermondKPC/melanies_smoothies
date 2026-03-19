c9304a35270eed83c761e69fe1ebda80b85b5643
tgermondKPC <thomas.germond@kpconsulting.fr>
2026-03-19 15:37:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingredients_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_with=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
