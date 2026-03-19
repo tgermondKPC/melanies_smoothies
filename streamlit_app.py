86f20c988a8c308e20355b4adbe08b6ecd20ea0e
tgermondKPC <thomas.germond@kpconsulting.fr>
2026-03-19 15:46:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
     #st.write(ingredients_string)
 
     my_insert_stmt = """Insert into smoothies.public.orders(ingredients, name_on_order) values ('"""+ ingredients_string +"""', '"""+ name_on_order +"""') """
@@ -48,8 +50,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered !', icon="✅")
 
-# New section to display smoothiefroot nutrition information
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
